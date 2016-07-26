776011eb6d31389dc4c2bf3bd09c6615b6925f6b
mygethub-99 <tomakeitcount@gmail.com>
2016-07-26 10:24:20 -0500

    Update settings.py
    updated web client id for the gamechanger-1260 from the workmanapp-server id.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ created July, 2016
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = '732887115129-o4p2h7pc5vmf79qmda5e7j10nb4pmhvv.apps.googleusercontent.com'
+WEB_CLIENT_ID = '840403803055-6qpukl41m6rrdmmniln4s7pdofddl8cs.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
